a321dd33011f817d598b1688086f4fdba4488388
FilipeJrd <filipejrd@hotmail.com>
2015-10-31 14:51:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var util = require('util');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port =  process.env.PORT || 8080;        // set our port
+var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
