17dd62c55f18434404273bf55b687b015fcf95e3
Filipe Jordão <filipejrd@hotmail.com>
2015-10-31 13:57:25 -0300

    fodase

---
+++ DB/dbcomms.cs.js
@@ -0,0 +1,6 @@
+/**
+ * Created by filipe on 10/31/15.
+ */
+var mongoose = require('mongoose');
+
+module.exports = Foodtruck;
--- server.js
+++ server.js
@@ -5,7 +5,8 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+var util = require('util');
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,27 +16,44 @@ var port =  process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
+mongoose.connect('mongodb://localhost/projetao');
+var db =  mongoose.connection;
+db.on('error', console.error);
+
+
+var ftSchema = new mongoose.Schema({
+    nome: String,
+    local: String
+});
+
+var Foodtruck = mongoose.model('Foodtruck',ftSchema);
+
+
+
+
+
 var router = express.Router();              // get an instance of the express Router
 
+router.use(bodyParser.json());
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/foodtrucks', function(req, res, next) {
-    res.json(
-        {
-            foodtruck: {
-                 name : 'cuzão', endereco: {
-                    rua: 'cu de daniel',
-                    numero:'dentro'
-                }
-            }
-        }
-    );
-    next();
+router.get('/foodtrucks',function(req, res) {
+    Foodtruck.find({}, function (err, docs) {
+        res.json(docs);
+    });
 });
 
-router.post('/ft',function(req,res,next){
-	res.json(req.body);
-	next();
+router.post('/foodtrucks',function(req,res){
+    var ft =  new Foodtruck({
+        nome: req.body.nome,
+        local: req.body.local
+    });
+    ft.save(function(err){
+       if(err) throw err;
+        console.log('Foodtruck registered');
+    });
 });
+
+
 
 // more routes for our API will happen here
 
@@ -46,5 +64,6 @@ app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
+
 app.listen(port);
 console.log('Magic happens on port ' + port);
