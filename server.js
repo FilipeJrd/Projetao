021843d2eda5b0eb04f85cc8ea3f511c0d28bed0
FilipeJrd <filipejrd@hotmail.com>
2015-10-31 14:45:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ var port =  process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-mongoose.connect('mongodb://localhost/projetao');
+//mongoose.connect('mongodb://localhost/projetao');
+mongoose.connect('mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/');
 var db =  mongoose.connection;
 db.on('error', console.error);
 
